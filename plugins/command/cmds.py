97d0294feaacc30c8b9180d9b53ca4004e59f5d4
JrzGerard12 <jrzowner1@gmail.com>
2025-06-03 14:41:03 -0600

    Actualizar cmds.py

--- plugins/command/cmds.py
+++ plugins/command/cmds.py
@@ -9,6 +9,6 @@ def start(_, m):
         photo='https://i.ibb.co/ynZ4nVVR/IMG-20250602-033447-327.jpg',
         caption=f"""<b>¡Bienvenido(a), @{m.from_user.username}!</b>
 
-<b>Gracias por usar nuestros servicios. Descubre nuestra amplia gama de herramientas y comandos disponibles. Utiliza los botones a continuación para explorar todas las funcionalidades que ofrecemos.</b>""",
+<b>Gracias por usar nuestros servicios. Descubre nuestra amplia gama de herramientas y comandos disponibles.</b>""",
         reply_markup=commd(m.from_user.id)
     )
